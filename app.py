7aaf597192941650c87b967b6225978a06770738
zeugy <rpdev@protonmail.com>
2022-11-04 16:54:37 +0100

    2ème visu

--- app.py
+++ app.py
@@ -4,23 +4,20 @@ import helpers.graphs as hg
 import plotly.express as px
 import os
 
-app = Dash(__name__, external_scripts=["https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.5/MathJax.js?config=TeX-MML-AM_CHTML" ])
+app = Dash(__name__)
 app.title = "Dashboard JO"
 
-
 app.layout = html.Div(className= 'main', children=[
-    html.H1("Dessine moi les JO 🏃"),
-    
+    html.H1("Dessine moi les JO 🏃"),    
     html.Div(className = 'details', children=[
         html.Div(id='flag-container'),
         dcc.Dropdown(options =[{'label': 'France', 'value': 'FRA'},
-       {'label': 'Italie', 'value': 'ITA'},
-       {'label': 'Royaume-Uni', 'value': 'GBR'},
-       {'label': 'Allemagne', 'value': 'GER'}],value = 'FRA', id='demo-dropdown'),
+       
+       {'label': 'Royaume-Uni', 'value': 'GBR'}],value = 'FRA', id='demo-dropdown'),
         html.P(id = 'title-container')
     ]),
     html.Div(id='dd-output-container'),
-    
+    html.Div(id='top10-output-container'),    
     html.Footer(" © Tous droits réservés - Cosmodev")
     ]
 )
@@ -61,6 +58,20 @@ def change_title(country):
 )
 def change_flag(country):
     return html.Img(src='assets/flag_{}.png'.format(country)),
+
+@app.callback(
+    Output('top10-output-container','children'),
+    Input('demo-dropdown','value')
+)
+def update_output(country):
+    df = hg.display2(country=country)    
+    fig = px.bar(df, x=df.athlete_full_name, y=df.total_metals, color = df.total_metals, title="Les 10 meilleurs athètes et leurs médailles obtenus", labels={
+                     "total_metals": "Médailles remportés",
+                     "athlete_full_name": "Athlètes" })
+    return dcc.Graph(
+        figure= fig
+    )
+    
     
 
 if __name__ == '__main__':
--- helpers/graphs.py
+++ helpers/graphs.py
@@ -17,3 +17,22 @@ def display1(country):
     df = df.groupby(by="game_year").size().reset_index(name='medals')
 
     return df
+
+def display2(country):
+    df_hosts = pd.read_csv(r"{}/archive/olympic_hosts.csv".format(os.getcwd()))
+    df_medals = pd.read_csv(r"{}/archive/olympic_results.csv".format(os.getcwd())) 
+
+    #fusionner df_medals & df_hosts pour filter les JO d'étés 
+    
+    df = df_medals.merge(df_hosts, left_on='slug_game', right_on='game_slug')
+
+    #filtrer : on garde les athlètes de GB médaillés au jeux d'étés
+    df.query("country_3_letter_code == @country and game_season == 'Summer'", inplace=True)
+    df.dropna(subset=['medal_type'], inplace = True)
+    df_medal_by_athlete = df.groupby(['athlete_full_name','medal_type']).size().reset_index(name="total_metals")
+    s_medals = df.groupby(['athlete_full_name']).size().nlargest(10)
+    df_medal_by_athlete['total_medals'] = df_medal_by_athlete['athlete_full_name'].map(s_medals)
+    df_medal_by_athlete.dropna(subset=['total_medals'], inplace = True)
+    df_medal_by_athlete.sort_values(['total_metals','total_medals'], ascending= False, inplace=True)
+    
+    return df_medal_by_athlete
